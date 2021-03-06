a096a1a2914515d3ef9f45901d7ecb39222f9f1c
Jefferson <jefferson88_hernandez@hotmail.es>
2021-03-05 23:20:22 -0500

    se creo el index

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    if(isset($_SESSION["carrito"])){
+        $numCarrito=$_SESSION["carrito"];
+    }else{
+        //Si no existe, cr�ela y as�gnele el timestamp de Unix por medio de la funci�n time(). Guarde el archivo.
+    }
+    $numCarrito=3;
+    require_once('clases/db.php');
+    require_once('clases/carrito.php');
+    $importeCarrito=Carrito::totalCarrito($numCarrito);
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Indice</title>
+<style>
+</style>
+
+</head>
+<body>
+
+<header>
+</header>
+
+<section>
+
+</section>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
